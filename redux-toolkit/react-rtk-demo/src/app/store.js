9cee7c9ab4d6984595fba37ca9210f63e2588ed9
shabeerv <shabrv@gmail.com>
2022-11-25 10:58:39 +0400

    react redux

---
+++ redux-toolkit/react-rtk-demo/src/app/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import cakeReducer from '../features/cake/cakeSlice'
+import icecreamReducer from '../features/icecream/icecreamSlice'
+//const reduxLogger = require('redux-logger')
+//const logger = reduxLogger.createLogger()
+import userReducer from '../features/user/userSlice'
+
+const store = configureStore({
+    reducer: {
+        cake: cakeReducer,
+        icecream: icecreamReducer,
+        user: userReducer,
+    },
+    //middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+})
+
+export default store
