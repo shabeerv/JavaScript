9accbd2ff273eeb736603ca154ebcde4d360c58d
shabeerv <shabrv@gmail.com>
2022-12-20 10:50:57 +0400

    React testing

--- react-testing/src/App.tsx
+++ react-testing/src/App.tsx
@@ -1,11 +1,22 @@
 import './App.css';
-import { Application } from './components/application/application';
+import { Counter } from './components/Counter/Counter';
+import { MuiMode } from './components/Mui/MuiMode';
+import { AppProviders } from './providers/AppProvider';
+// import { Application } from './components/application/application';
+// import { Skills }  from './skills/skills';
 
 function App() {
+  // const skills = ['HTML', 'CSS', 'JavaScript']
   return (
-    <div className="App">
-      <Application />
-    </div>
+      <AppProviders>
+        <div className="App">
+          <MuiMode />
+        </div>
+      </AppProviders>
+
+      // {/* <Application />
+      // <Skills skills={skills} /> */}
+      // {/* <Counter /> */}
   );
 }
 
--- react-testing/src/components/application/application.test.tsx
+++ react-testing/src/components/application/application.test.tsx
@@ -15,13 +15,36 @@ describe("Applicaiton", () => {
         })
         expect(sectionHeading).toBeInTheDocument()
 
+        const paragraphElement = screen.getByText("All fields are mandatory")
+        expect(paragraphElement).toBeInTheDocument()
+
+        const closeElement = screen.getByTitle('close')
+        expect(closeElement).toBeInTheDocument()
+
+        const customElement = screen.getByTestId('custom-element')
+        expect(customElement).toBeInTheDocument()
+
         const nameElement = screen.getByRole("textbox", {
             name: "Name",
         })
         expect(nameElement).toBeInTheDocument()
 
-        const nameElement2 = screen.getByLabelText('Name')
+        const nameElement2 = screen.getByLabelText('Name', {
+            selector: "input",
+        })
         expect(nameElement2).toBeInTheDocument()
+
+        const nameElement3 = screen.getByPlaceholderText('Fullname')
+        expect(nameElement3).toBeInTheDocument()
+
+        const nameElement4 = screen.getByDisplayValue('Shabeer')
+        expect(nameElement4).toBeInTheDocument()
+
+        const imageElement = screen.getByAltText('a person with a laptop')
+        expect(imageElement).toBeInTheDocument()
+
+        const termsElement2 = screen.getByLabelText('I agree to the terms and conditions')
+        expect(termsElement2).toBeInTheDocument();
 
         const bioElement = screen.getByRole("textbox", {
             name: "Bio",
